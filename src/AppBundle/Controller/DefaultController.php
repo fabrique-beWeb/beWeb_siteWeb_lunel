90872a65cc1ce05d8662563ef2ff98522d1bb05e
ccadars <ccadars.beweb@gmail.com>
2017-01-23 17:08:27 +0100

    creation promo et trombi

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,17 +3,18 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="home")
+     * @Template("default/index.html.twig")
      */
     public function getHome()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        //return $this->render('default/index.html.twig');
     }
 }
