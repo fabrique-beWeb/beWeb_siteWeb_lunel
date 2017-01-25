aff686fe57341754f969d3c30c48029c0e0026d2
vien <vien@vien-Vostro-3559>
2017-01-25 14:47:11 +0100

    rectification master

--- src/AppBundle/Controller/ContrleurDesVues.php
+++ src/AppBundle/Controller/ContrleurDesVues.php
@@ -1,11 +1,5 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ 
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -15,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ContrleurDesVues extends Controller {
     
     /**
-     * @Route("/promo")
+     * @Route("/promo",name = "promo")
      * @Template("default/promo.html.twig")
      */
     function viewPromo (){
