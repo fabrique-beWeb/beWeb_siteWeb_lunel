35fe35d622346c0844cb50497f26fe3c36f37523
vien <vien@vien-Vostro-3559>
2017-01-27 11:16:39 +0100

    methode affichage promotion (a fini)

--- src/AppBundle/Controller/ControleurCrud.php
+++ src/AppBundle/Controller/ControleurCrud.php
@@ -12,24 +12,42 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+class ControleurCrud extends Controller {
+///////////////   Affichage Section 
 
-class ControleurCrud extends Controller{
-    
-  /**
- * @Route("/", name="home")
- * @Template("default/index.html.twig")
- */
-    public function getTextSection(){
-         ///////   Affichage Section 
-      $em = $this->getDoctrine();
-      $texts = $em->getRepository("AppBundle:Texte")->findAll();
-       ///////   Affichage parte 
-      $em2 = $this->getDoctrine();
-      $part = $em2->getRepository("AppBundle:Partenaire")->findAll();
-      //// Retourne vers  twig
-      return array ('sectionText' => $texts, "parte" => $part ); 
+    /** ///////////////   Affichage Header Page 
+     * @Route("/", name="home")
+     * @Template("default/index.html.twig")
+     */
+    public function getHome() {
+        //nit Doctrine
+        $em = $this->getDoctrine();
+        // Utilisaton de du Repository  pour aller chercher tout mes entiter Texte de ma db 
+        $texts = $em->getRepository("AppBundle:Texte")->findAll();
+        ///////   Affichage parte 
+        $em2 = $this->getDoctrine();
+        $part = $em2->getRepository("AppBundle:Partenaire")->findAll();
+        //// Retourne nos entiter  vers  twig
+        return array('sectionText' => $texts, "parte" => $part);
+    }
+
+    ///////////////   Affichage Section Promo
+
+    /** /////////////// Affichage Home Promo
+     * @Route("/promo/home", name="homePromo")
+     * @Template("default/promo.html.twig")
+     */
+    public function getHomePromo() {
+        
+        $em = $this->getDoctrine();
+        
+        $promosLunel = $em->getRepository("AppBundle:Promos")->findByfkville(1);
+        ///////   Affichage parte 
+        $em2 = $this->getDoctrine();
+        $promosBezier = $em2->getRepository("AppBundle:Promos")->findByfkville(2);
+        //// Retourne vers  twig
+        return array('promosLunel' => $promosLunel, "promosBezier" => $promosBezier);
         
     }
-   
-    
+
 }
