43fa4ec678902af3657636396cfe21fc89e7a982
vien <vien@vien-Vostro-3559>
2017-02-03 14:12:16 +0100

    Tout les bouttons admin message ok

--- src/AppBundle/Form/MessageType.php
+++ src/AppBundle/Form/MessageType.php
@@ -3,27 +3,33 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MessageType extends AbstractType
-{
+class MessageType extends AbstractType {
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add('mail')
-                ->add('objet')
-                ->add('message')        ;
+                ->add('objet',TextType::class,array(
+                                                'attr' => array(
+                                                                        'placeholder' => 'Object')))
+                ->add('message', TextareaType::class, array(
+                                                                                        'attr' => array(
+                                                                                                                'placeholder' => 'Text')))
+                ->add('Valider', SubmitType::class);
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Message'
         ));
@@ -32,10 +38,8 @@ class MessageType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'appbundle_message';
     }
-
 
 }
