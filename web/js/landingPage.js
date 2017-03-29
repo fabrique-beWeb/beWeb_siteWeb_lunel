19864ec2f27919d895ad5c50e7b432c5dc0274f3
m4tieu <mathieu.becerra.beweb@gmail.com>
2017-03-29 09:53:33 +0200

    Carousel image + titre, Js sur la navbar, modif css complet (couleur)

--- web/js/landingPage.js
+++ web/js/landingPage.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    
+
 ////// Bouton Poopup Inscription 
     $(".insctrip").css("display", "none");
     $("#btnClose").click(function () {
-    $(".insctrip").css("display", "none");
+        $(".insctrip").css("display", "none");
     });
 
     $("#validationInscrip").click(function () {
-        
+
         ///////Recuperation champ inscription 
         ////Recuperachamption mail  
         var $mail = $("input[id=mail]").val();
@@ -38,29 +38,29 @@ $(document).ready(function () {
         });
 
     });
-        //////Requet ajax pour Contacte
+    //////Requet ajax pour Contacte
     $("#validationConta").click(function () {
-        
+
         ///////Recuperation champ inscription 
         ////Recuperachamption mail  
         var $mailConta = $("input[id=mailConta]").val();
         var $objConta = $("input[id=objConta]").val();
         var $messConta = $("textarea[id=messConta]").val();
-      
+
         //////Requet ajax pour inscription
         $.ajax({
             url: 'http://www.beweb.fr/contacteValid', ///// La ressource ciblée
             data: {"mailConta": $mailConta,
-                       "objConta":  $objConta,
-                        "messConta" : $messConta
-                        },
+                "objConta": $objConta,
+                "messConta": $messConta
+            },
             type: 'get',
             dataType: "json",
             /**
              * Le paramètre data n'est plus renseigné, nous ne faisons plus passer de variable
              */
             success: function () {
-                
+
 
             },
             complete: function () {
@@ -73,3 +73,28 @@ $(document).ready(function () {
     });
 
 });
+// Scroll navbar fixe on top //
+var positionElementInPage = $('.navbar').offset().top;
+$(window).resize(function () {
+    positionElementInPage = $('.navbar').offset().top;
+});
+$(window).scroll(
+        function () {
+            if ($(window).scrollTop() > positionElementInPage) {
+                // fixed
+                $('.navbar').addClass("fixedTop");
+            } else {
+                // unfixed
+                $('.navbar').removeClass("fixedTop");
+            }
+        }
+
+);
+//// Decalage taille de la barre nav //
+//$("#myNavbar a").click(function(){
+//    var lien = $(this).text();
+//    alert(lien);
+//    $("#" + lien).css({
+//        marginTop: "500px",
+//    })
+//});
